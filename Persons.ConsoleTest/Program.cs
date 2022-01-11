baed26cff648944d9fe34c62de29614799c7f4e4
Starinin Andrey <starininandrey@gmail.com>
2022-01-11 21:31:32 +0300

    Add console test

---
+++ Persons.ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Persons.DataModel;
+using Persons.Lib;
+
+namespace Persons.ConsoleTest
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var db = new DB();
+            var persons = db.GetAllPersons();
+            ShowPersons(persons);
+        }
+
+        private static void ShowPersons(List<Person> persons)
+        {
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person.Id}: {person.LastName} {person.FirstName}, {person.Age}; is delete: {person.IsDelete}");
+            }
+        }
+    }
+}
